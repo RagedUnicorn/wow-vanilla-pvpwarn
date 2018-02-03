2d7a2f6477b19ce0050ded273dd9d3507bc5f788
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-02-03 23:32:13 +0100

    Add shouldhavesound tests for category rogue

--- Test/PVPW_TestRogueDe.lua
+++ Test/PVPW_TestRogueDe.lua
@@ -43,7 +43,28 @@ function _G.__PVPW__TEST_ROGUE_DE__Test()
   -- silence logging to errorlevel
   mod.logger.logLevel = 1
 
+  mod.testHelper.TestShouldHaveASoundTestForEachSpell(me, className)
+  mod.testHelper.TestShouldHaveASoundDownTestForSpellsThatFade(me, className)
+
   me.RunAll()
+end
+
+--[[
+  tests if a sound testcase can be found for each registered spell in PVPW_SpellMap
+]]--
+function _G.__PVPW__TEST_ROGUE_DE__Test_Sound()
+  mod.testReporter.StartTestRun("global_rogue_de_sound")
+  mod.testReporter.StartTestClass(className)
+
+  mod.testHelper.TestShouldHaveASoundTestForEachSpell(me, className)
+end
+
+
+function _G.__PVPW__TEST_ROGUE_DE__Test_Sound_Down()
+  mod.testReporter.StartTestRun("global_rogue_de_sound_down")
+  mod.testReporter.StartTestClass(className)
+
+  mod.testHelper.TestShouldHaveASoundDownTestForSpellsThatFade(me, className)
 end
 
 --[[
