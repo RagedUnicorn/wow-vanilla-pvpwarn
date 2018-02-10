6a072bf7d10f487065460aac1fecb1b0ca5bd57d
Michael Wiesendanger <raged.unicorn.dev@gmail.com>
2018-02-10 12:01:45 +0100

    Add parse and parsedown tests for category mage

--- Test/PVPW_TestMageDe.lua
+++ Test/PVPW_TestMageDe.lua
@@ -45,6 +45,8 @@ function _G.__PVPW__TEST_MAGE_DE__Test()
 
   mod.testHelper.TestShouldHaveASoundTestForEachSpell(me, className)
   mod.testHelper.TestShouldHaveASoundDownTestForSpellsThatFade(me, className)
+  mod.testHelper.TestShouldHaveAParseTestForEachSpell(me, className)
+  mod.testHelper.TestShouldHaveAParseDownTestForSpellsThatFade(me, className)
 
   me.RunAll()
 end
@@ -61,6 +63,20 @@ function _G.__PVPW__TEST_MAGE_DE__Test_Sound_Down()
   mod.testReporter.StartTestClass(className)
 
   mod.testHelper.TestShouldHaveASoundDownTestForSpellsThatFade(me, className)
+end
+
+function _G.__PVPW__TEST_MAGE_DE__Test_Parse()
+  mod.testReporter.StartTestRun("global_mage_de_parse")
+  mod.testReporter.StartTestClass(className)
+
+  mod.testHelper.TestShouldHaveAParseTestForEachSpell(me, className)
+end
+
+function _G.__PVPW__TEST_MAGE_DE__Test_Parse_Down()
+  mod.testReporter.StartTestRun("global_mage_de_parse_down")
+  mod.testReporter.StartTestClass(className)
+
+  mod.testHelper.TestShouldHaveAParseDownTestForSpellsThatFade(me, className)
 end
 
 --[[
@@ -99,6 +115,32 @@ function me.RunAll(playManual)
   if not playManual then
     mod.testReporter.PlayTestQueueWithDelay()
   end
+
+  mod.testReporter.AddToTestQueue(me.TestParseEisblock)
+  mod.testReporter.AddToTestQueue(me.TestParseDownEisblock)
+  mod.testReporter.AddToTestQueue(me.TestParseVerwandlung)
+  mod.testReporter.AddToTestQueue(me.TestParseVerwandlungSchwein)
+  mod.testReporter.AddToTestQueue(me.TestParseVerwandlungKuh)
+  mod.testReporter.AddToTestQueue(me.TestParseVerwandlungKroete)
+  mod.testReporter.AddToTestQueue(me.TestParseBlinzeln)
+  mod.testReporter.AddToTestQueue(me.TestParseFeuerzauberschutz)
+  mod.testReporter.AddToTestQueue(me.TestParseDownFeuerzauberschutz)
+  mod.testReporter.AddToTestQueue(me.TestParseFrostzauberschutz)
+  mod.testReporter.AddToTestQueue(me.TestParseDownFrostzauberschutz)
+  mod.testReporter.AddToTestQueue(me.TestParseGegenzauberZumSchweigenGebracht)
+  mod.testReporter.AddToTestQueue(me.TestParseManaschild)
+  mod.testReporter.AddToTestQueue(me.TestParseDownManaschild)
+  mod.testReporter.AddToTestQueue(me.TestParseEisbarriere)
+  mod.testReporter.AddToTestQueue(me.TestParseDownEisbarriere)
+  mod.testReporter.AddToTestQueue(me.TestParseFrostnova)
+  mod.testReporter.AddToTestQueue(me.TestParseArkaneMacht)
+  mod.testReporter.AddToTestQueue(me.TestParseDownArkaneMacht)
+  mod.testReporter.AddToTestQueue(me.TestParseHervorrufung)
+  mod.testReporter.AddToTestQueue(me.TestParseGeistesgegenwart)
+  mod.testReporter.AddToTestQueue(me.TestParseDownGeistesgegenwart)
+  mod.testReporter.AddToTestQueue(me.TestParseVerbrennung)
+  mod.testReporter.AddToTestQueue(me.TestParseDownVerbrennung)
+  mod.testReporter.AddToTestQueue(me.TestParseDruckwelle)
 
   mod.testReporter.PlayTestQueue()
 end
@@ -300,5 +342,255 @@ function me.TestSoundDruckwelle()
     className,
     "TestSoundDruckwelle",
     "druckwelle"
+  )
+end
+
+function me.TestParseEisblock()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseEisblock",
+    "eisblock",
+    "$player$ bekommt Eisblock.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownEisblock()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownEisblock",
+    "eisblock",
+    "Eisblock schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseVerwandlung()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseVerwandlung",
+    "verwandlung",
+    "Ihr seid von Verwandlung betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
+  )
+end
+
+function me.TestParseVerwandlungSchwein()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseVerwandlungSchwein",
+    "verwandlung_schwein",
+    "Ihr seid von Verwandlung: Schwein betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
+  )
+end
+
+function me.TestParseVerwandlungKuh()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseVerwandlungKuh",
+    "verwandlung_kuh",
+    "Ihr seid von Verwandlung: Kuh betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
+  )
+end
+
+function me.TestParseVerwandlungKroete()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseVerwandlungKroete",
+    "verwandlung_kroete",
+    "Ihr seid von Verwandlung: Kröte betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
+  )
+end
+
+function me.TestParseBlinzeln()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseBlinzeln",
+    "blinzeln",
+    "$player$ bekommt Blinzeln.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseFeuerzauberschutz()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseFeuerzauberschutz",
+    "feuerzauberschutz",
+    "$player$ bekommt Feuerzauberschutz.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownFeuerzauberschutz()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownFeuerzauberschutz",
+    "feuerzauberschutz",
+    "Feuerzauberschutz schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseFrostzauberschutz()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseFrostzauberschutz",
+    "frostzauberschutz",
+    "$player$ bekommt Frostzauberschutz.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownFrostzauberschutz()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownFrostzauberschutz",
+    "frostzauberschutz",
+    "Frostzauberschutz schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseGegenzauberZumSchweigenGebracht()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseGegenzauberZumSchweigenGebracht",
+    "gegenzauber_zum_schweigen_gebracht",
+    "Ihr seid von Gegenzauber - zum Schweigen gebracht betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
+  )
+end
+
+function me.TestParseManaschild()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseManaschild",
+    "manaschild",
+    "$player$ bekommt Manaschild.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownManaschild()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownManaschild",
+    "manaschild",
+    "Manaschild schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseEisbarriere()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseEisbarriere",
+    "eisbarriere",
+    "$player$ bekommt Eisbarriere.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownEisbarriere()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownEisbarriere",
+    "eisbarriere",
+    "Eisbarriere schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseFrostnova()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseFrostnova",
+    "frost_nova",
+    "Ihr seid von Frostnova betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
+  )
+end
+
+function me.TestParseArkaneMacht()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseArkaneMacht",
+    "arkane_macht",
+    "$player$ bekommt Arkane Macht.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownArkaneMacht()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownArkaneMacht",
+    "arkane_macht",
+    "Arkane Macht schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseHervorrufung()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseHervorrufung",
+    "hervorrufung",
+    "$player$ bekommt Hervorrufung.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseGeistesgegenwart()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseGeistesgegenwart",
+    "geistesgegenwart",
+    "$player$ bekommt Geistesgegenwart.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownGeistesgegenwart()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownGeistesgegenwart",
+    "geistesgegenwart",
+    "Geistesgegenwart schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseVerbrennung()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseVerbrennung",
+    "verbrennung",
+    "$player$ bekommt Verbrennung.",
+    mod.testHelper.eventTypeSpellPeriodicHostilePlayerBuffs
+  )
+end
+
+function me.TestParseDownVerbrennung()
+  mod.testHelper.TestParseDown(
+    className,
+    "TestParseDownVerbrennung",
+    "verbrennung",
+    "Verbrennung schwindet von $player$.",
+    mod.testHelper.eventTypeSpellAuraGoneOther
+  )
+end
+
+function me.TestParseDruckwelle()
+  mod.testHelper.TestParse(
+    className,
+    "TestParseDruckwelle",
+    "druckwelle",
+    "Ihr seid von Druckwelle betroffen.",
+    mod.testHelper.eventTypeSpellPeriodicSelfDamage
   )
 end
